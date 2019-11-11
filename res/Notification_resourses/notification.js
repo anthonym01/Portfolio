b5313b26975dd32218f5b86b409b9e27369e740a
Samuel Matheson <samuelmatheson15@gmail.com>
2019-11-11 11:19:37 -0500

    Notifications to be added

--- res/Notification_resourses/notification.js
+++ res/Notification_resourses/notification.js
@@ -1,120 +1,28 @@
+/*
+    Author: Samuel A. Matheson
+    Email:samuelmatheson15@gmail.com
+                        ║\
+                        ║▒\
+                        ║▒▒\
+                        ║░▒║
+                        ║░▒║
+                        ║░▒║
+                        ║░▒║
+                        ║░▒║ 
+                        ║░▒║
+                        ║░▒║
+                        ║░▒║
+                        ▓▓▓▓
+                        [█▓]
+                        [█▓]
+                        [█▓]
+                        [█▓]
+            U steal the code, U get new holes
+*/
 
-window.addEventListener('load',function(){//window loads
-    if(localStorage.getItem("SamuelAMatheson_cfg")){config.load()}
-    else{config.validate()}
-    UI.initalize();
-    UI.unblurse();//must happen last, blursing takes the place of a loading screen
-});
-
-var config = {/*  Configuaration manager  */
-    data:{
-        current_view:"home",//
-    },
-    save:function(){//Save the config file
-        localStorage.setItem("SamuelAMatheson_cfg",JSON.stringify(config.data));
-        console.log('config saved: ');
-        console.table(config.data);
-    },
-    load:function(){//Load the config file into memory
-        config.data=JSON.parse(localStorage.getItem("SamuelAMatheson_cfg"));
-        console.log('config Loaded: ');
-        console.table(config.data);
-        this.validate();
-    },
-    validate:function(){//validate configuration file
-        console.log('Config is being validated');
-        var configisvalid = true;
-        if(typeof(this.data.current_view)!=='undefined'){
-
-        }else{
-            this.data.current_view='home'
-        }
-        if(!configisvalid){
-            console.log('config was found to be invalid and will be overwritten');
-            this.save();//Save new confog because old config is no longer valid
-        }else{
-            console.log('config was found to be valid');
-        }
-    },
-    delete:function(){//Does not delete the file itself. Just sets it to empty
-        localStorage.clear("SamuelAMatheson_cfg");
-        console.log('config deleted: ');
-        console.table(config.data);
-        this.validate();
-    }
-}
-
-let UI = {
-    initalize:function(){//Put the listeners in after the page loads so people cant break things by clicking
-        document.getElementById('service_btn').addEventListener('click',this.navigate.service);
-        document.getElementById('about_btn').addEventListener('click',this.navigate.about);
-        document.getElementById('project_btn').addEventListener('click',this.navigate.project);
-        document.getElementById('home_btn').addEventListener('click',this.navigate.home);
-        document.getElementById('contact_btn').addEventListener('click',this.navigate.contact);
-        this.startup();
-    },
-    startup:function(){//Takes the page to the last view screen, add functionality to scroll to last point in the page
-        switch(config.data.current_view){
-            case "home": this.navigate.home();break;
-            case "about": this.navigate.about();break;
-            case "project": this.navigate.project();break;
-            case "contact": this.navigate.contact();break;
-            case "service": this.navigate.service();break;
-            default: this.navigate.home();
-        }
-    },
-    navigate:{//Navigate to a view
-        service:function(){
-            document.getElementById('service_view').style.display='block';
-            document.getElementById('about_view').style.display='none';
-            document.getElementById('project_view').style.display='none';
-            document.getElementById('home_view').style.display='none';
-            document.getElementById('contact_view').style.display='none';
-            config.data.current_view="service";
-            config.save();
-        },
-        about:function(){
-            document.getElementById('service_view').style.display='none';
-            document.getElementById('about_view').style.display='block';
-            document.getElementById('project_view').style.display='none';
-            document.getElementById('home_view').style.display='none';
-            document.getElementById('contact_view').style.display='none';
-            config.data.current_view="about";
-            config.save();
-        },
-        project:function(){
-            document.getElementById('service_view').style.display='none';
-            document.getElementById('about_view').style.display='none';
-            document.getElementById('project_view').style.display='block';
-            document.getElementById('home_view').style.display='none';
-            document.getElementById('contact_view').style.display='none';
-            config.data.current_view="project";
-            config.save();
-        },
-        home:function(){
-            document.getElementById('service_view').style.display='none';
-            document.getElementById('about_view').style.display='none';
-            document.getElementById('project_view').style.display='none';
-            document.getElementById('home_view').style.display='block';
-            document.getElementById('contact_view').style.display='none';
-            config.data.current_view="home";
-            config.save();
-        },
-        contact:function(){
-            document.getElementById('service_view').style.display='none';
-            document.getElementById('about_view').style.display='none';
-            document.getElementById('project_view').style.display='none';
-            document.getElementById('home_view').style.display='none';
-            document.getElementById('contact_view').style.display='block';
-            config.data.current_view="contact";
-            config.save();
-        },
-    },
-    unblurse:function(){//Un-sade the page after loading is complete
-        document.getElementById('page_shade').style.backgroundColor="rgba(0,0,0,0)";
-        setTimeout(()=>{document.getElementById('page_shade').style.display="none";},400);
-    }
-}
+/*
+    If u dont nunderstand my code barf just use 'notify.new(notifiaction_title,notification_body);' to call the notification
+*/
 
 let notify = {  /*  Notification handler  */
     preset_height:22,//2 more than the height in the css
